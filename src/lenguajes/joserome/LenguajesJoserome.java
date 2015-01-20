176ed28ef8fa621a7328119db897f88adb44738b
T-107 <T-107@PC256>
2015-01-19 19:29:03 -0600

    Mi primer push a mi repositorio

---
+++ src/lenguajes/joserome/LenguajesJoserome.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lenguajes.joserome;
+
+/**
+ *
+ * @author T-107
+ */
+public class LenguajesJoserome {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("imprime programa de java 2");
+    }
+    
+}
