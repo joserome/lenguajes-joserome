48e218656d43e4e5d5b638f327130ef3b31ef0f9
T-301 <T-301@PC341>
2015-02-12 19:42:28 -0600

    arreglos

--- src/unitec/proyecto/geometria/cuadrado.java
+++ src/unitec/proyecto/geometria/cuadrado.java
@@ -12,6 +12,17 @@ package unitec.proyecto.geometria;
 public class cuadrado {
      private float lado;
     
+     //en la clase cuadrado agregar un constructor que inicialize el atributo lado
+     public cuadrado(float lado){
+     this.lado=lado;
+     }
+     //se agrega este porq ya agregamos un constructor y es una de las reglas
+     public cuadrado(){
+     
+     }
+     
+     
+     
     float calcularArea(){
     
     float area=lado*lado;
